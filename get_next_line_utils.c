db7b089dde60ab1b145cca69d8866a0c17c92d2b
Swenne Offerhaus <swofferh@student.codam.nl>
2020-01-10 22:23:58 +0100

    strlcpy, strlen, buf_strct, etc

---
+++ get_next_line_utils.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_utils.c                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/01/08 21:53:24 by swofferh       #+#    #+#                */
+/*   Updated: 2020/01/10 21:58:14 by swofferh      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+t_list	*get_buffer(t_list **lst, int fd)
+{
+	t_list	*node;
+	t_list	*new;
+
+	node = *lst;
+	while (node)
+	{
+		if (node->fd == fd)
+			return (node);
+		node = node->next;
+	}
+	new = (t_list *)malloc(sizeof(t_list));
+	if (new == NULL)
+		return (NULL);
+	new->content = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (new->content == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->size = 0;
+	new->fd = fd;
+	new->next = *lst;
+	*lst = new;
+	return (new);
+}
+
+char		*copy_buffer(char *old, char *new, size_t n)
+{
+	char	*next;
+	size_t	len;
+
+	if (old == NULL)
+	{
+		next = (char *)malloc(sizeof(char) * (n + 1));
+		if (next == NULL)
+			return (NULL);
+		ft_strncpy(next, new, n);
+	}
+	else
+	{
+		len = scan_index(old, '\0');
+		next = (char *)malloc(sizeof(char) * (len + n + 1));
+		if (next == NULL)
+			return (NULL);
+		ft_strncpy(next, old, len);
+		free(old);
+		ft_strncpy(next + len, new, n);
+	}
+	return (next);
+}
+
+char		*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < len)
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	dst[index] = '\0';
+	return (dst);
+}
+
+//looks for end of line (eol)
+size_t		scan_index(char *str, char c)
+{
+	size_t	index;
+
+	index = 0;
+	while (str[index])
+	{
+		if (str[index] == c)
+			break ;
+		index++;
+	}
+	return (index);
+}
