d525ab44be5451279e7e3830e92f752d5d31dd5c
Swenne Offerhaus <swofferh@student.codam.nl>
2020-01-20 19:15:57 +0100

    next level testing file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/08 22:08:52 by swofferh       #+#    #+#                */
-/*   Updated: 2020/01/18 18:16:11 by swofferh      ########   odam.nl         */
+/*   Updated: 2020/01/20 19:14:12 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,44 @@ int	main(void)
 	int		fd1;
 	int		fd2;
 	int		fd3;
+	int		fd4;
+	int		fd5;
 	char	*line;
 	int		state1;
 	int		state2;
 	int		state3;
+	int		state0;
 
 	state1 = 1;
 	state2 = 1;
 	state3 = 1;
+	state0 = 1;
 	fd1 = open("oscar-wilde.txt", O_RDONLY);
 	fd2 = open("get_next_line.h", O_RDONLY);
-	fd3 = open("get_next_line.pdf", O_RDONLY);
-	//close(42);
-	while (state1 > 0 && state2 > 0 && state3 > 0)
+	fd3 = open("get_next_line.c", O_RDONLY);
+	fd4 = open("", O_RDONLY);
+	fd5 = 42;
+	close(42);
+	printf("\n[testing for errors]\n\n");
+	printf("fd[%d] return[%d]\n", fd4, get_next_line(fd4, &line));
+	printf("fd[%d] return[%d]\n", fd5, get_next_line(fd4, &line));
+	printf("\n[testing single fd]\n\n");
+	while (state1 > 0)
 	{
 		state1 = get_next_line(fd1, &line);
 		printf("fd[%d] ret[%d] line->%s\n", fd1, state1, line);
 		if (state1 >= 0)
 			free(line);
+	}
+	printf("\n[testing multiple fds]\n\n");
+	while (state2 > 0 && state3 > 0)
+	{
 		state2 = get_next_line(fd2, &line);
-		printf("fd[%d] ret[%d] line->%s\n", fd2, state2, line);
+		printf("fd[%d] ret[%d] line ->%s\n", fd2, state2, line);
 		if (state2 >= 0)
 			free(line);
 		state3 = get_next_line(fd3, &line);
-		printf("fd[%d] ret[%d] line->%s\n", fd3, state3, line);
+		printf("fd[%d] ret[%d] line ->%s\n", fd3, state3, line);
 		if (state3 >= 0)
 			free(line);
 	}
