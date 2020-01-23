942580af5e0915177bf4eb5fa6ee4b836ee482e9
Swenne Offerhaus <swofferh@student.codam.nl>
2020-01-23 20:08:56 +0100

    only free, strncpy and scan_index

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,67 +6,35 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/08 21:53:24 by swofferh       #+#    #+#                */
-/*   Updated: 2020/01/20 19:14:34 by swofferh      ########   odam.nl         */
+/*   Updated: 2020/01/23 18:54:57 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// updating struct values
-t_list	*get_buffer(int fd, t_list **lst)
+void		free_buffer(int fd, t_list **lst)
 {
 	t_list	*node;
-	t_list	*new;
+	t_list	*recursive;
 
 	node = *lst;
-	while (node)
+	if (node->fd == fd)
 	{
-		if (node->fd == fd)// checking / scaning for existing fd
-			return (node);
-		node = node->next;
+		*lst = node->next;
+		free(node->content);
+		free(node);
 	}
-	new = (t_list *)malloc(sizeof(t_list));
-	if (new == NULL)
-		return (NULL);
-	new->content = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (new->content == NULL)
+	else if (node->next->fd == fd)
 	{
-		free(new);
-		return (NULL);
-	}
-	new->size = 0; // looking for new ones - fds (to handle multiples)
-	new->fd = fd;
-	new->next = *lst;
-	*lst = new;
-	return (new);
-}
-
-char		*copy_buffer(char *old, char *new, size_t n)
-{
-	char	*next; //why call it next?
-	size_t	len;
-
-	if (old == NULL)
-	{
-		next = (char *)malloc(sizeof(char) * (n + 1));
-		if (next == NULL)
-			return (NULL);
-		ft_strncpy(next, new, n);
+		recursive = node->next;
+		node->next = node->next->next;
+		free(recursive->content);
+		free(recursive);
 	}
 	else
-	{
-		len = scan_index(old, '\0');
-		next = (char *)malloc(sizeof(char) * (len + n + 1));
-		if (next == NULL)
-			return (NULL);
-		ft_strncpy(next, old, len);
-		free(old);
-		ft_strncpy(next + len, new, n);
-	}
-	return (next);
+		free_buffer(fd, &node->next);
 }
 
-// one constant unchangable state
 char		*ft_strncpy(char *dst, const char *src, size_t n)
 {
 	size_t	index;
@@ -81,7 +49,6 @@ char		*ft_strncpy(char *dst, const char *src, size_t n)
 	return (dst);
 }
 
-// looks for the end of line (eol)
 size_t		scan_index(char *str, char c)
 {
 	size_t	index;
