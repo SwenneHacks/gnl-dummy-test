7beb0000715780053c9a3314675f61e1e191326c
Swenne Offerhaus <swofferh@student.codam.nl>
2020-01-25 16:22:34 +0100

    undo static char

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/08 21:54:19 by swofferh       #+#    #+#                */
-/*   Updated: 2020/01/23 20:11:40 by swofferh      ########   odam.nl         */
+/*   Updated: 2020/01/23 22:40:50 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,26 @@
 #  define BUFFER_SIZE 32
 # endif
 
-typedef enum			e_state
+typedef enum		e_state
 {
 	READ_LINE = 1,
 	END_FILE = 0,
 	ERROR = -1,
 	LOOP = 2
-}						t_state;
+}					t_state;
 
-typedef struct			s_list
+typedef struct		s_list
 {
-	char				*content;
-	size_t				size;
-	int					fd;
-	struct s_list		*next;
-}						t_list;
+	char			*content;
+	size_t			size;
+	int				fd;
+	struct s_list	*next;
+}					t_list;
 
-void					free_buffer(int fd, t_list **lst);
-char					*ft_strncpy(char *dst, const char *src, size_t n);
-int						get_next_line(int fd, char **line);
-size_t					scan_index(char *s, char c);
+int					get_next_line(int fd, char **line);
+void				free_buffer(int fd, t_list **lst);
+char				*copy_buffer(char *old, char *new, size_t n);
+char				*ft_strncpy(char *dst, const char *src, size_t n);
+size_t				scan_index(char *str, char c);
 
 #endif
