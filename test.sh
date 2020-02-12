f6734c637c41a0bf784b9dac230e6888c5a199dd
Swenne Offerhaus <swofferh@student.codam.nl>
2020-02-12 23:07:52 +0100

    deleted extra files in directory

--- test.sh
+++
@@ -1,1 +0,0 @@
- gcc -Wall -Wextra -Werror get_next_line.c get_next_line_utils.c main.c -g -o gnl
--- test_gnl.sh
+++
@@ -1,2 +0,0 @@
- gcc -Wall -Wextra -Werror get_next_line.c get_next_line_utils.c test_main.c -g -o test_output 
- ./test_output
