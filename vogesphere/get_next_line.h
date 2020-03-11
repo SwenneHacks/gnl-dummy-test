0b7be21a3e6e1c8ce86b21584ca88f4b16e5a365
Swenne Offerhaus <swofferh@student.codam.nl>
2020-03-12 00:00:56 +0100

    codam

---
+++ vogesphere/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/01/08 21:54:19 by swofferh       #+#    #+#                */
+/*   Updated: 2020/03/02 16:10:49 by swofferh      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+typedef enum		e_state
+{
+	ONE_LINE = 1,
+	END_FILE = 0,
+	ERROR = -1,
+	LOOP = 2
+}					t_state;
+
+int					get_next_line(int fd, char **line);
+char				*copy_buffer(char *old, char *new, int n);
+char				*ft_strncpy(char *dst, const char *src, int n);
+int					ft_strclen(char *str, char c);
+
+#endif
